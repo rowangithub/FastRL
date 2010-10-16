014c22ec7b3a41de1655ed1f8a1a2bb74d1c191c
Aijun Bai <baj@mail.ustc.edu.cn>
2010-10-16 18:53:04 +0800

    change gamma to 1.0 - 1.0e-4

--- src/td-agent.h
+++ src/td-agent.h
@@ -19,7 +19,7 @@
 class TemporalDifferenceAgent: public EpsilonAgent {
 public:
 	static const double alpha = 0.15; //learning rate - which is somehow good according to empirical resutls
-	static const double gamma = 1.0 - 1.0e-6; //constant step parameter
+	static const double gamma = 1.0 - 1.0e-4; //constant step parameter
 
 public:
 	TemporalDifferenceAgent(const std::string name, const bool test): EpsilonAgent(test), name_(name) {
